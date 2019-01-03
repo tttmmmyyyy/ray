14a974fdfa93fa34f212679d9f499da422e32643
tttmmmyyyy <39012639+tttmmmyyyy@users.noreply.github.com>
2019-01-03 14:41:03 +0900

    コメント修正

--- src/scatter_record.rs
+++ src/scatter_record.rs
@@ -1,6 +1,6 @@
 use crate::pdf::SingularPdf;
 
-/// bundled informations to calculate the result of scattering, i.e., the next ray.
+/// Informations to calculate the scattered ray.
 pub struct ScatterRecord {
     // A pdf to sample a direction of the scattered ray.
     pub pdf: SingularPdf,
