1aef33560991acf9f20d0348ad1af9c624d3f2f7
tttmmmyyyy <39012639+tttmmmyyyy@users.noreply.github.com>
2019-01-05 20:13:19 +0900

    Teapotシーン変更

--- src/scenes/teapot.rs
+++ src/scenes/teapot.rs
@@ -3,7 +3,6 @@ use ray::aliases::Vec3;
 use ray::background::AmbientLight;
 use ray::camera::Camera;
 use ray::hitable::bvh_node::BvhNode;
-use ray::hitable::empty::Empty;
 use ray::hitable::hitable_list::HitableList;
 use ray::hitable::obvh::OBVH;
 use ray::hitable::rectangle::Rectangle;
@@ -23,51 +22,61 @@ use std::sync::Arc;
 pub fn scene(aspect_ratio: f32) -> Scene {
     let mut objs = Vec::<Arc<Hitable>>::new();
     const L: f32 = 20.0;
+    let checker = Arc::new(CheckerTexture::new(
+        Arc::new(ConstantTexture::new(&Vec3::new(1.0, 1.0, 1.0))),
+        Arc::new(ConstantTexture::new(&Vec3::new(0.8, 0.8, 0.8))),
+        0.8,
+        &Vec3::new(0.0, 0.1, 0.1),
+    ));
+    let lambert_checker = Arc::new(Lambertian::new(checker.clone()));
+    let _phong_checker = Arc::new(Phong::new(checker.clone(), 0.5, 0.5, 50, 0.5));
+    let wall_mat = lambert_checker.clone();
+    // Lambertian::boxed(ConstantTexture::boxed(&Vec3::new(0.9, 0.9, 0.9))),
     objs.push(Arc::new(Rectangle::new(
-        &Vec3::new(-L, -0.01, -L),
-        &Vec3::new(0.0, -0.01, 2.0 * L),
+        &Vec3::new(-L, -0.0, -L),
+        &Vec3::new(0.0, -0.0, 2.0 * L),
         &Vec3::new(2.0 * L, -0.01, 0.0),
-        Arc::new(Lambertian::new(Arc::new(CheckerTexture::new(
-            Arc::new(ConstantTexture::new(&Vec3::new(1.0, 1.0, 1.0))),
-            Arc::new(ConstantTexture::new(&Vec3::new(0.15, 0.15, 0.50))),
-            0.8,
-        )))),
+        wall_mat.clone(),
+        0.1,
+    ))); // floor
+    let wall_z = -3.0;
+    objs.push(Arc::new(Rectangle::new(
+        &Vec3::new(-L, -L, wall_z),
+        &Vec3::new(2.0 * L, 0.0, 0.0),
+        &Vec3::new(0.0, 2.0 * L, 0.0),
+        wall_mat.clone(),
         // Lambertian::boxed(ConstantTexture::boxed(&Vec3::new(0.9, 0.9, 0.9))),
         0.1,
     ))); // floor
-    objs.push(Arc::new(Sphere::new(
-        &Vec3::new(0.0, 16.0, 0.0),
-        4.0,
+    let light_power = 10.0;
+    let light_radius = 6.0;
+    let light = Arc::new(Sphere::new(
+        &Vec3::new(0.0, 22.5, light_radius),
+        light_radius,
         Arc::new(DiffuseLight::new(Arc::new(ConstantTexture::new(
-            &Vec3::new(2.5, 2.5, 2.5),
+            &Vec3::new(light_power, light_power, light_power),
         )))),
-    ))); // light
+    ));
+    objs.push(light.clone()); // light
     let lambert = Arc::new(Lambertian::new(Arc::new(ConstantTexture::new(&Vec3::new(
         232.0 / 255.0,
         200.0 / 255.0,
         0.5,
     )))));
     let _glass = Arc::new(Glass::new(2.2, 0.0));
-    let k = 0.5;
-    let _phong = Arc::new(Phong::new(
-        Arc::new(ConstantTexture::rgb(
-            k * 232.0 / 255.0,
-            k * 200.0 / 255.0,
-            k * 0.5,
-        )),
-        Arc::new(ConstantTexture::rgb(
-            (1.0 - k) * 1.0,
-            (1.0 - k) * 1.0,
-            (1.0 - k) * 1.0,
-        )),
+    let phong = Arc::new(Phong::new(
+        Arc::new(ConstantTexture::rgb(232.0 / 255.0, 200.0 / 255.0, 0.5)),
+        0.5,
+        0.5,
         50,
-        k,
+        0.5,
     ));
     let teapot = &mut ObjFile::from_file(Path::new("res/teapot.obj"))
         .unwrap()
         .groups[0];
+    teapot.unify_vertex();
     teapot.set_smooth_normals();
-    let teapot = Arc::new(BvhNode::new(teapot.to_triangles(lambert.clone()), 0.0, 1.0));
+    let teapot = Arc::new(BvhNode::new(teapot.to_triangles(phong.clone()), 0.0, 1.0));
     // let bunny = &mut ObjFile::from_file(Path::new("res/bunny.obj"))
     //     .unwrap()
     //     .groups[0];
@@ -89,7 +98,7 @@ pub fn scene(aspect_ratio: f32) -> Scene {
     objs.push(teapot);
     // objs.push(bunny);
     let objs = Arc::new(HitableList::new(objs));
-    let look_from = Vec3::new(-5.0, 6.0, -5.0);
+    let look_from = Vec3::new(0.0, 3.0, 10.0);
     let look_at = Vec3::new(0.0, 1.0, 0.0);
     let dist_to_focus = 5.0;
     let vfov = 40.0;
@@ -104,11 +113,12 @@ pub fn scene(aspect_ratio: f32) -> Scene {
         0.0, // time_0
         1.0, // time_1
     );
-    let bg = Arc::new(AmbientLight::new(&Vec3::new(0.75, 0.85, 1.0)));
+    // let bg = Arc::new(AmbientLight::new(&Vec3::new(0.75, 0.85, 1.0)));
+    let bg = Arc::new(AmbientLight::new(&Vec3::new(0.0, 0.0, 0.0)));
     Scene {
         hitables: objs,
-        importance: Arc::new(Empty::new()),
-        importance_weight: 0.0,
+        importance: light.clone(),
+        importance_weight: 0.4,
         camera: camera,
         bg: bg,
     }
